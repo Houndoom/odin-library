041d7010111c8718a656e7b1b708e617b91c8ff7
Houndoom <therealhoundoom@gmail.com>
2022-07-08 20:51:17 +0800

    Add function to show only favourites/read

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 let myLibrary = [];
 
-function Book(title,author,year,publisher,pages,read) {
+/* Book object */
+
+function Book(title,author,year,publisher,pages) {
   this.title = title;
   this.author = author;
   this.year = year;
   this.publisher = publisher;
   this.pages = pages;
-  this.read = read;
+  this.read = false;
+  this.favourited = false;
 }
 
-function addBookToLibrary(title,author,year,publisher,pages,read) {
-  const newBook = new Book(title,author,year,publisher,pages,read);
+/* Function to add book */
+
+function addBookToLibrary(title,author,year,publisher,pages) {
+  const newBook = new Book(title,author,year,publisher,pages);
   myLibrary.push(newBook);
   return;
 }
 
-function removeAllCards() {
-  const allBooks = document.querySelectorAll('.card:not(#card-prototype)');
-  allBooks.forEach(n => n.remove());
-  return;
-}
+/* Function to display book */
 
 function displayBooksInLibrary() {
   removeAllCards();
@@ -29,6 +30,7 @@ function displayBooksInLibrary() {
     const newBook = prototype.cloneNode(true);
 
     newBook.removeAttribute("id");
+    newBook.classList.add("card");
     newBook.setAttribute("data-book-number", i);
     newBook.querySelector(".id-number").textContent = i;
     newBook.querySelector(".title").textContent = book.title;
@@ -36,23 +38,48 @@ function displayBooksInLibrary() {
     newBook.querySelector(".year>.book-data").textContent = book.year;
     newBook.querySelector(".publisher>.book-data").textContent = book.publisher;
     newBook.querySelector(".pages>.book-data").textContent = book.pages;
+
+    if (book.favourited) {
+      newBook.classList.add("favourited");
+      const favourite = newBook.querySelector(".favourite > img");
+      favourite.src = "images/star-fav.svg";
+    }
+
+    if (book.read) {
+      newBook.classList.add("read");
+      const read = newBook.querySelector(".read > img");
+      read.src = "images/book-open-variant.svg";
+    }
+
+    // Add event listeners to the buttons
+    newBook.querySelector(".favourite").addEventListener("click",toggleFavourite);
+    newBook.querySelector(".read").addEventListener("click",toggleRead);
+    newBook.querySelector(".delete-button").addEventListener("click",deleteBook)
     document.querySelector(".library").appendChild(newBook);
   }
 
-  const allTitles = document.querySelectorAll(".card:not(#card-prototype) .book-data");
+  // textFit all long text fields on card
+  const allTitles = document.querySelectorAll(".card .book-data");
   allTitles.forEach(e => textFit(e));
+  
+  const toggleButtons = document.querySelectorAll('.header-buttons button');
+  toggleButtons.forEach(e => {
+    if (e.classList.contains("pressed")) {
+      const buttonImage = e.querySelector('img');
+      e.classList.remove("pressed");
+      buttonImage.src = buttonImage.src.replace("-white","");
+    }
+  });
   return;
 }
 
-const formExit = document.getElementById('form-exit');
-
-formExit.addEventListener('click',closeNewBookWindow);
-
-function closeNewBookWindow() {
-  const formAddNewBook = document.getElementById('form-add-new-book');
-  formAddNewBook.style.display = '';
+function removeAllCards() {
+  const allBooks = document.querySelectorAll('.card');
+  allBooks.forEach(n => n.remove());
   return;
 }
+
+/* Buttons to open and close form to add book */
 
 const formOpen = document.getElementById('form-open');
 
@@ -70,6 +97,18 @@ function openNewBookWindow() {
   return;
 }
 
+const formExit = document.getElementById('form-exit');
+
+formExit.addEventListener('click',closeNewBookWindow);
+
+function closeNewBookWindow() {
+  const formAddNewBook = document.getElementById('form-add-new-book');
+  formAddNewBook.style.display = '';
+  return;
+}
+
+/* Button to add book details from form */
+
 const submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click',addNewBook);
@@ -81,11 +120,23 @@ function addNewBook() {
   const publisherInput = document.getElementById('publisher-input');
   const pagesInput = document.getElementById('pages-input');
 
-  if (!titleInput.value) {
+  if (!titleInput.validity.valid) {
     setError('title-input','Please enter a title');
     titleInput.addEventListener('input',() => clearError('title-input'));
-  } else {
-    addBookToLibrary(titleInput.value,authorInput.value || '-',yearInput.value || '-',publisherInput.value || '-',pagesInput.value || '-',false);
+  } 
+  
+  if (!yearInput.validity.valid) {
+    setError('year-input','Please enter a number between 0 and 9999');
+    yearInput.addEventListener('input',() => checkError('year-input'));
+  }
+
+  if (!pagesInput.validity.valid) {
+    setError('pages-input','Please enter a number between 0 and 999999');
+    pagesInput.addEventListener('input',() => checkError('pages-input'));
+  }
+
+  if (titleInput.validity.valid && yearInput.validity.valid && pagesInput.validity.valid) {
+    addBookToLibrary(titleInput.value,authorInput.value || '-',yearInput.value || '-',publisherInput.value || '-',pagesInput.value || '-');
     displayBooksInLibrary();
     clearAllFields();
   }
@@ -106,13 +157,94 @@ function clearError(inputID) {
   return;
 }
 
+function checkError(inputID) {
+  const selectedInput = document.getElementById(inputID);
+  if (selectedInput.validity.valid) {
+    selectedInput.classList.remove('field-error');
+    selectedInput.nextElementSibling.textContent = "";
+    selectedInput.removeEventListener('input',checkError('year-input'));
+  }
+  return;
+}
+
 function clearAllFields() {
   const allFields = document.querySelectorAll('form input');
   allFields.forEach(e => e.value = "");
   return;
 }
 
-function testFit() {
-  textFit(document.querySelector('[data-book-number="1"] .title'));
+/* Functions for card buttons */
+
+function deleteBook() {
+  const parentCard = this.parentElement;
+  const cardNumber = parseInt(parentCard.querySelector(".id-number").textContent) - 1;
+  myLibrary.splice(cardNumber,1);
+  displayBooksInLibrary();
+}
+
+function toggleFavourite() {
+  const favourite = this.querySelector("img");
+  let parentCard = this.parentElement;
+  while (!parentCard.classList.contains("card")) parentCard = parentCard.parentElement;
+  const cardNumber = parseInt(parentCard.querySelector(".id-number").textContent) - 1;
+  if (parentCard.classList.contains("favourited")) {
+    parentCard.classList.remove("favourited");
+    favourite.src = "images/star-outline.svg";
+    myLibrary[cardNumber].favourited = false;
+  } else {
+    parentCard.classList.add("favourited");
+    favourite.src = "images/star-fav.svg";
+    myLibrary[cardNumber].favourited = true;
+  }
   return;
+}
+
+function toggleRead() {
+  const read = this.querySelector("img");
+  let parentCard = this.parentElement;
+  while (!parentCard.classList.contains("card")) parentCard = parentCard.parentElement;
+  const cardNumber = parseInt(parentCard.querySelector(".id-number").textContent) - 1;
+  if (parentCard.classList.contains("read")) {
+    parentCard.classList.remove("read");
+    read.src = "images/book-open-blank-variant.svg";
+    myLibrary[cardNumber].read = false;
+  } else {
+    parentCard.classList.add("read");
+    read.src = "images/book-open-variant.svg";
+    myLibrary[cardNumber].read = true;
+  }
+  return;
+}
+
+/* Buttons to show only selected cards */
+
+const showFavourites = document.getElementById("show-favourites");
+const showRead = document.getElementById("show-read");
+
+showFavourites.addEventListener("click",() => toggleShow(showFavourites,'favourited'));
+showRead.addEventListener("click", () => toggleShow(showRead,'read'));
+
+function toggleShow(node,className) {
+  const buttonImage = node.querySelector('img');
+  const nonShown = document.querySelectorAll(`.card:not(.${className})`);
+  if (node.classList.contains("pressed")) {
+    node.classList.remove("pressed");
+    buttonImage.src = buttonImage.src.replace("-white","");
+    nonShown.forEach(e => {
+      e.classList.remove("hide");
+      const cardNumber = parseInt(e.querySelector(".id-number").textContent) - 1;
+    })
+  } else {
+    node.classList.add("pressed");
+    buttonImage.src = buttonImage.src.replace(".svg","-white.svg");
+    nonShown.forEach(e => {
+      e.classList.add("hide");
+      const cardNumber = parseInt(e.querySelector(".id-number").textContent) - 1;
+    })
+  }
+  return;
+}
+
+function toggleShowRead() {
+
 }
